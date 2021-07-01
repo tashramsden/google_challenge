de9e17450d2c188aab993107c2abe54c5a931623
Tash Ramsden <natasha.ramsden18@gmail.com>
2021-07-01 10:04:10 +0100

    Part 3 functional but doesn't pass tests

--- python/src/video_player.py
+++ python/src/video_player.py
@@ -255,7 +255,7 @@ class VideoPlayer:
             return
 
     def search_videos_tag(self, video_tag):
-        """Display all videos whose tags contains the provided tag.
+        """Display all videos whose tags contains the provided tag .
         Args:
             video_tag: The video tag to be used in search.
         """
